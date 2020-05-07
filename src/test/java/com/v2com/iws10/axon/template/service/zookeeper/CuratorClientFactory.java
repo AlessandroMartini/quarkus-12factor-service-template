d838e94e26d18fd207744b5cda57018f24ae3e2b
AMartini <amartini@v2com.mobi>
2020-05-07 15:42:26 -0300

    Adequando configurações.

--- src/test/java/com/v2com/iws10/axon/template/service/zookeeper/CuratorClientFactory.java
+++ src/test/java/com/v2com/iws10/axon/template/service/zookeeper/CuratorClientFactory.java
@@ -6,17 +6,20 @@ import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryNTimes;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 public class CuratorClientFactory {
 
-  private String path = "127.0.0.1:2181";
+  @ConfigProperty(name = "zookeeper.address", defaultValue="localhost:2181")
+  String zookeeperAddress;
+
   private int sleepMsBetweenRetries = 100;
   private int maxRetries = 30;
 
   @Produces
   public CuratorFramework newClient() {
     RetryPolicy retryPolicy = new RetryNTimes(maxRetries, sleepMsBetweenRetries);
-    CuratorFramework client = CuratorFrameworkFactory.newClient(path, retryPolicy);
+    CuratorFramework client = CuratorFrameworkFactory.newClient(zookeeperAddress, retryPolicy);
     return client;
   }
 
